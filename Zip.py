d3359ed3672df0c444ad19f88b98d148bbd4fc6f
Manoj Kumar R <119232010+Manojkumar-mk41@users.noreply.github.com>
2023-10-25 17:31:52 +0530

    Add files via upload

---
+++ Zip.py
@@ -0,0 +1,8 @@
+username = ['User1', 'User2', 'User3']
+password = [1234, 4122, 7890]
+names = ['Luffy', 'Zoro', 'Sanji']
+
+my_dict = dict(zip(username,password))
+my_list = list(zip(username,password,names))
+print(my_dict)
+print(my_list)
